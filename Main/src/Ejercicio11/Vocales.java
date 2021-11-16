29f016e38397170d49ae649803de08a668193a81
MarlonDValencia <marlondvalencia@gmail.com>
2021-11-16 17:03:34 -0500

    Another random commit

--- Main/src/Ejercicio11/Vocales.java
+++ Main/src/Ejercicio11/Vocales.java
@@ -1,19 +1,25 @@
 package Ejercicio11;
-
 import java.util.*;
 public class Vocales {
-
-    public static void count(String cadena){
-        for(char c: cadena.toCharArray()){
-            System.out.println(c);
+    public static int[] count(String cadena){
+        int counter[] = {0,0,0,0,0};
+            for(int i = 0;i<cadena.length();i++){
+                switch (String.valueOf((cadena.toLowerCase()).charAt(i))) {
+                    case "a" -> counter[0]++;
+                    case "e" -> counter[1]++;
+                    case "i" -> counter[2]++;
+                    case "o" -> counter[3]++;
+                    case "u" -> counter[4]++;
+                }
+            }
+            return counter;
         }
-    }
-
     public static void main(String[] args) {
+        System.out.println("Ingresa una frase");
         Scanner captura = new Scanner(System.in);
         String txt = captura.nextLine();
-        int longitud = txt.length();
-        count("AAAAAAAAAAAAAAAAAAAAA");
-        System.out.println();
+        int[] x = count(txt);
+        System.out.println("La frase tiene una longitud de " +txt.length()+" caracteres.");
+        System.out.println("Contador de vocales:\nA: "+x[0]+"\nE: "+x[1]+"\nI: "+x[2]+"\nO: "+x[3]+"\nU: "+x[4]);
     }
 }
---
+++ Main/src/Ejercicio12/Diferencia.java
@@ -0,0 +1,13 @@
+package Ejercicio12;
+
+import java.util.Scanner;
+
+public class Diferencia {
+    public static void main(String[] args) {
+        System.out.println("Ingresa la primera palabra");
+        Scanner captura = new Scanner(System.in);
+        String txt1 = captura.nextLine();
+        System.out.println("Ingresa la primera palabra");
+        String txt2 = captura.nextLine();
+    }
+}
